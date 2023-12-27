ad77a6a890498e29a309427d31f97f7188d4bc2c
Joseph Lander <josephclander@gmail.com>
2023-12-27 15:56:09 +0000

    Add links to users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import styles from '../page.module.css';
 import users from './user_data.json';
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -7,7 +8,14 @@ export default function Page() {
       <h1 className={styles.main}>Users Page</h1>
       <ul>
         {users &&
-          users.map((user) => <li key={user.id}>{user?.name?.first_name}</li>)}
+          users.map((user) => (
+            <li key={user.id}>
+              <Link href={`/users/${user.id}`}>
+                {user?.name?.first_name.slice(0, 1).toUpperCase()}.{' '}
+                {user?.name?.last_name}
+              </Link>
+            </li>
+          ))}
       </ul>
     </>
   );
