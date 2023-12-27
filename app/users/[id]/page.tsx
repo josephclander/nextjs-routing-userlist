2a3a7dcc0ee491d79a227ef779c94dab5b62d308
Joseph Lander <josephclander@gmail.com>
2023-12-27 15:58:55 +0000

    Add defensive code to user page

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -4,16 +4,16 @@ import users from '../user_data.json';
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const user = users.filter((item) => item.id === id)[0];
-  const { name, job_title, email, phone } = user;
+  const { name, job_title, email, phone } = user || {};
   return (
     <>
       <h1 className={styles.main}>
-        {user && name.first_name + ' ' + name.last_name}
+        {user && name && name?.first_name + ' ' + name?.last_name}
       </h1>
       <ul>
-        <li>{job_title}</li>
-        <li>{email}</li>
-        <li>{phone}</li>
+        {job_title && <li>{job_title}</li>}
+        {email && <li>{email}</li>}
+        {phone && <li>{phone}</li>}
       </ul>
     </>
   );
